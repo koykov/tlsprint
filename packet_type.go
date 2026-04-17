e40dd3bb3e32a6026db73174b13c278a3f32dae8
Konstantin Voykov <koykov0@gmail.com>
2026-04-17 22:37:54 +0300

    parser: code fmt issues

--- packet_type.go
+++ packet_type.go
@@ -1,6 +1,9 @@
 package tlsprint
 
-import "math"
+import (
+	"encoding/binary"
+	"math"
+)
 
 type PacketType uint8
 
@@ -17,7 +20,29 @@ func (pt PacketType) String() string {
 	return ptyps[pt]
 }
 
-var ptyps [math.MaxUint8]string
+func (ctx *Ctx) parsePacketType() error {
+	raw := ctx.raw[ctx.off:]
+	if len(raw) < 2 {
+		return ErrTooShort
+	}
+	pt := binary.LittleEndian.Uint16(raw[ctx.off : ctx.off+2])
+	ctx.off += 2
+	switch pt {
+	case pt01:
+		ctx.ptyp = PacketTypeClientHello
+	case pt02:
+		ctx.ptyp = PacketTypeServerHello
+	default:
+		return ErrUnknownPacketType
+	}
+	return nil
+}
+
+var (
+	ptyps [math.MaxUint8]string
+	pt01  = binary.LittleEndian.Uint16([]byte("01"))
+	pt02  = binary.LittleEndian.Uint16([]byte("02"))
+)
 
 func init() {
 	ptyps[PacketTypeUnknown] = "UNKNOWN"
