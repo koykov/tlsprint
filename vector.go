b8a6a2b9cf9994ea35054e7ba652c7ba8eaaebb1
Konstantin Voykov <koykov0@gmail.com>
2026-04-22 23:28:56 +0300

    code fmt issues

--- vector.go
+++ vector.go
@@ -2,8 +2,6 @@ package tlsvector
 
 import (
 	"io"
-
-	"github.com/koykov/byteconv"
 )
 
 type Interface interface {
@@ -44,42 +42,6 @@ type vector struct {
 
 func New() Interface {
 	return &vector{}
-}
-
-func (vec *vector) Parse(raw []byte) (err error) {
-	vec.Reset()
-	vec.raw = raw
-	var off uint32
-
-	if off, err = vec.parseRecordHeader(off); err != nil {
-		return
-	}
-	if off, err = vec.parseHandshakeHeader(off); err != nil {
-		return
-	}
-	if off, err = vec.parseTLSVersion(off); err != nil {
-		return
-	}
-	if off, err = vec.parseClientRandom(off); err != nil {
-		return err
-	}
-	if off, err = vec.parseSessionID(off); err != nil {
-		return
-	}
-	if off, err = vec.parseCipherSuites(off); err != nil {
-		return
-	}
-	if off, err = vec.parseCompressionMethod(off); err != nil {
-		return
-	}
-	if off, err = vec.parseExtensions(off); err != nil {
-		return
-	}
-	return
-}
-
-func (vec *vector) ParseString(raw string) error {
-	return vec.Parse(byteconv.S2B(raw))
 }
 
 func (vec *vector) RecordType() RecordType {
