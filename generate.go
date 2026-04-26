ee4bc1ab6f7a17f975f0d98bb80d3f998e9eca5c
Konstantin Voykov <koykov0@gmail.com>
2026-04-26 15:24:30 +0300

    generate: add extension repo generation

--- generate.go
+++ generate.go
@@ -1,3 +1,4 @@
 package tlsvector
 
 //go:generate tlsvecgen --cipher-suites=https://www.iana.org/assignments/tls-parameters/tls-parameters-4.csv --dst=cipher_suites_repo.go
+//go:generate tlsvecgen --extensions=https://www.iana.org/assignments/tls-extensiontype-values/tls-extensiontype-values-1.csv --dst=extensions_repo.go
