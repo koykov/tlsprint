fb12d097fbd9db66eb5e7fbc95128210fe302020
Konstantin Voykov <koykov0@gmail.com>
2026-04-26 16:40:01 +0300

    generate: add elliptic curves

--- generate.go
+++ generate.go
@@ -2,3 +2,4 @@ package tlsvector
 
 //go:generate tlsvecgen --cipher-suites=https://www.iana.org/assignments/tls-parameters/tls-parameters-4.csv --dst=cipher_suites_repo.go
 //go:generate tlsvecgen --extensions=https://www.iana.org/assignments/tls-extensiontype-values/tls-extensiontype-values-1.csv --dst=extensions_repo.go
+//go:generate tlsvecgen --elliptic-curves=https://www.iana.org/assignments/tls-parameters/tls-parameters-8.csv --dst=elliptic_curves_repo.go
