a456113719fc0a40b4335ef648b59f53404f51e4
Konstantin Voykov <koykov0@gmail.com>
2026-05-06 21:54:51 +0300

    ja3: cover with bench

--- vector_test.go
+++ vector_test.go
@@ -20,3 +20,23 @@ func TestVector(t *testing.T) {
 		})
 	}
 }
+
+func BenchmarkVector(b *testing.B) {
+	for i := 0; i < len(stages); i++ {
+		st := &stages[i]
+		b.Run(st.key, func(b *testing.B) {
+			b.Run("client hello/ja3", func(b *testing.B) {
+				vec := New()
+				err := vec.Parse(st.flows[0])
+				if err != nil {
+					b.Fatal(err)
+				}
+				b.ReportAllocs()
+				b.ResetTimer()
+				for j := 0; j < b.N; j++ {
+					_ = vec.JA3()
+				}
+			})
+		})
+	}
+}
