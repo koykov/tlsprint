f8d733b62568db19f1b6dfb6d75436e564227a2b
Konstantin Voykov <koykov0@gmail.com>
2026-05-07 22:58:06 +0300

    ja3: test/bench hierarchy

--- vector_test.go
+++ vector_test.go
@@ -8,14 +8,16 @@ func TestVector(t *testing.T) {
 	for i := 0; i < len(stages); i++ {
 		st := &stages[i]
 		t.Run(st.key, func(t *testing.T) {
-			t.Run("client hello/ja3", func(t *testing.T) {
+			t.Run("client hello", func(t *testing.T) {
 				vec := New()
 				err := vec.Parse(st.flows[0])
 				if err != nil {
 					t.Fatal(err)
 				}
-				h := vec.JA3()
-				t.Log(h)
+				t.Run("ja3", func(t *testing.T) {
+					h := vec.JA3()
+					t.Log(h)
+				})
 			})
 		})
 	}
@@ -25,17 +27,19 @@ func BenchmarkVector(b *testing.B) {
 	for i := 0; i < len(stages); i++ {
 		st := &stages[i]
 		b.Run(st.key, func(b *testing.B) {
-			b.Run("client hello/ja3", func(b *testing.B) {
+			b.Run("client hello", func(b *testing.B) {
 				vec := New()
 				err := vec.Parse(st.flows[0])
 				if err != nil {
 					b.Fatal(err)
 				}
-				b.ReportAllocs()
-				b.ResetTimer()
-				for j := 0; j < b.N; j++ {
-					_ = vec.JA3()
-				}
+				b.Run("ja3", func(b *testing.B) {
+					b.ReportAllocs()
+					b.ResetTimer()
+					for j := 0; j < b.N; j++ {
+						_ = vec.JA3()
+					}
+				})
 			})
 		})
 	}
