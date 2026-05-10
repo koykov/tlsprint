1c1df9f88aba627ad75f674ce5f05f4d77040658
Konstantin Voykov <koykov0@gmail.com>
2026-05-10 17:55:28 +0300

    ja4: version detect

--- ja4.go
+++ ja4.go
@@ -9,7 +9,22 @@ func (vec *vector) JA4String() string {
 	off := len(vec.buf)
 
 	vec.buf = append(vec.buf, 't')
-	vec.buf = append(vec.buf, vec.mver.Short()...)
+
+	var ver = vec.mver
+	for i := 0; i < len(vec.ext); i++ {
+		ext := &vec.ext[i]
+		if ext.Type == 0x002b && ext.Data.Len() > 1 {
+			data := ext.Data.Bytes()[1:]
+			for j := 0; j < len(data); j += 2 {
+				if data[j] == 0x03 {
+					ver = MessageVersion(uint16(data[j])<<8 | uint16(data[j+1]))
+					break
+				}
+			}
+			break
+		}
+	}
+	vec.buf = append(vec.buf, ver.Short()...)
 
 	_ = off
 	return ""
