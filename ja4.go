2b1dfa544e06541fcdfef609ca04ce351cc26afa
Konstantin Voykov <koykov0@gmail.com>
2026-05-11 22:33:35 +0300

    ja4: sni/cs part

--- ja4.go
+++ ja4.go
@@ -1,5 +1,7 @@
 package tlsvector
 
+import "strconv"
+
 func (vec *vector) JA4() string {
 	// todo implement me
 	return ""
@@ -11,6 +13,7 @@ func (vec *vector) JA4String() string {
 	vec.buf = append(vec.buf, 't')
 
 	var ver = vec.mver
+	var sni byte = 'i'
 	for i := 0; i < len(vec.ext); i++ {
 		ext := &vec.ext[i]
 		if ext.Type == 0x002b && ext.Data.Len() > 1 {
@@ -23,8 +26,25 @@ func (vec *vector) JA4String() string {
 			}
 			break
 		}
+		if ext.Type == 0x0000 && ext.Data.Len() > 0 {
+			sni = 'd'
+		}
 	}
 	vec.buf = append(vec.buf, ver.Short()...)
+	vec.buf = append(vec.buf, sni)
+
+	var chlen int
+	for i := 0; i < len(vec.chps); i++ {
+		cs := vec.chps[i]
+		if isGREASE(cs.Raw()) {
+			continue
+		}
+		chlen++
+	}
+	if chlen < 10 {
+		vec.buf = append(vec.buf, '0')
+	}
+	vec.buf = strconv.AppendInt(vec.buf, int64(chlen), 10)
 
 	_ = off
 	return ""
