f43b06d7c494882b7899f4bc510f2213656124a7
Konstantin Voykov <koykov0@gmail.com>
2026-05-13 23:55:05 +0300

    ja4: common raw string calculation method

--- ja4.go
+++ ja4.go
@@ -138,3 +138,133 @@ func (vec *vector) JA4String() string {
 
 	return byteconv.B2S(vec.buf[off:])
 }
+
+func (vec *vector) ja4s() (string, uint32, uint32) {
+	off := len(vec.buf)
+
+	// meta part
+
+	vec.buf = append(vec.buf, 't')
+
+	var ver = vec.mver
+	var sni byte = 'i'
+	for i := 0; i < len(vec.ext); i++ {
+		ext := &vec.ext[i]
+		if ext.Type == 0x002b && ext.Data.Len() > 1 {
+			data := ext.Data.Bytes()[1:]
+			for j := 0; j < len(data); j += 2 {
+				if data[j] == 0x03 {
+					ver = MessageVersion(uint16(data[j])<<8 | uint16(data[j+1]))
+					break
+				}
+			}
+			break
+		}
+		if ext.Type == 0x0000 && ext.Data.Len() > 0 {
+			sni = 'd'
+		}
+	}
+	vec.buf = append(vec.buf, ver.Short()...)
+	vec.buf = append(vec.buf, sni)
+
+	var chlen int
+	for i := 0; i < len(vec.chps); i++ {
+		cs := vec.chps[i]
+		if isGREASE(cs.Raw()) {
+			continue
+		}
+		chlen++
+	}
+	if chlen < 10 {
+		vec.buf = append(vec.buf, '0')
+	}
+	vec.buf = strconv.AppendInt(vec.buf, int64(chlen), 10)
+
+	var extlen int
+	var alpn = [2]byte{'0', '0'}
+	for i := 0; i < len(vec.ext); i++ {
+		ext := vec.ext[i]
+		if isGREASE(ext.Type.Raw()) {
+			continue
+		}
+		if ext.Type == 0x0010 {
+			ealpn := NewExtensionApplicationLayerProtocolNegotiation(ext.Data.Bytes())
+			var ok bool
+			ealpn.Each(func(protocol []byte) {
+				if len(protocol) >= 2 && !ok {
+					ok = true
+					alpn[0], alpn[1] = protocol[0], protocol[len(protocol)-1]
+				}
+			})
+		}
+		extlen++
+	}
+	if extlen < 10 {
+		vec.buf = append(vec.buf, '0')
+	}
+	vec.buf = strconv.AppendInt(vec.buf, int64(extlen), 10)
+	vec.buf = append(vec.buf, alpn[:]...)
+
+	// cipher suites part
+	var cslo, cshi uint16
+	cslo = uint16(len(vec.buf) - off)
+	vec.buf16 = vec.buf16[:0]
+	for i := 0; i < len(vec.chps); i++ {
+		cs := vec.chps[i].Raw()
+		if isGREASE(cs) {
+			continue
+		}
+		vec.buf16 = append(vec.buf16, cs)
+	}
+	slices.Sort(vec.buf16)
+	vec.buf = append(vec.buf, '_')
+	for i := 0; i < len(vec.buf16); i++ {
+		if i > 0 {
+			vec.buf = append(vec.buf, ',')
+		}
+		vec.buf = fmt.Appendf(vec.buf, "%04x", vec.buf16[i])
+	}
+	cshi = uint16(len(vec.buf) - off)
+
+	// extensions part
+	var exlo, exhi uint16
+	var sig *Extension
+	vec.buf16 = vec.buf16[:0]
+	for i := 0; i < len(vec.ext); i++ {
+		et := vec.ext[i].Type.Raw()
+		if isGREASE(et) || et == 0x0000 || et == 0x0010 {
+			continue
+		}
+		if et == 0x000d {
+			sig = &vec.ext[i]
+		}
+		vec.buf16 = append(vec.buf16, et)
+	}
+	slices.Sort(vec.buf16)
+	vec.buf = append(vec.buf, '_')
+	for i := 0; i < len(vec.buf16); i++ {
+		if i > 0 {
+			vec.buf = append(vec.buf, ',')
+		}
+		vec.buf = fmt.Appendf(vec.buf, "%04x", vec.buf16[i])
+	}
+
+	// signature algorithms part
+	if sig != nil {
+		vec.buf = append(vec.buf, '_')
+		ext := NewExtensionSignatureAlgorithms(sig.Data.Bytes())
+		var buf [2]byte
+		var c int
+		ext.Each(func(hash byte, sa SignatureAlgorithm) {
+			if c > 0 {
+				vec.buf = append(vec.buf, ',')
+			}
+			buf[0] = hash
+			buf[1] = sa.Raw()
+			vec.buf = fmt.Appendf(vec.buf, "%04x", buf[:])
+			c++
+		})
+	}
+
+	return byteconv.B2S(vec.buf[off:])
+}
